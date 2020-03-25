b84b5a8f81672a8728916c0e80e5e399076598f6
Anton Kablukov <antoha7@gmail.com>
2020-03-25 13:56:21 +0100

    basic functionality

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://api.themoviedb.org/3';
+
+export const API_KEY_3 = 'b3999c3f3a12db3d3325f190c7a5e44f';
+
+export const API_KEY_4 =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzk5OWMzZjNhMTJkYjNkMzMyNWYxOTBjN2E1ZTQ0ZiIsInN1YiI6IjVlN2EzN2U3ZDE4YjI0MDAxMzgzZDg0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CrF1NezmlzCAuEat5miZeA54Fo8okUDQPazc9-u6UHI';
---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import SortBy from './SortBy';
+
+export default class Filters extends React.Component {
+    render() {
+        const {
+            filters: { sort_by },
+            onChangeFilters,
+            onChangePage,
+            page
+        } = this.props;
+        return (
+            <form className="mb-3">
+                <SortBy sort_by={sort_by} onChangeFilters={onChangeFilters} />
+
+                <div className="btn-group">
+                    <button
+                        type="button"
+                        className="btn btn-light"
+                        disabled={page === 1}
+                        onClick={onChangePage.bind(null, page - 1)}
+                    >
+                        Next
+                    </button>
+
+                    <button
+                        type="button"
+                        className="btn btn-light"
+                        onClick={onChangePage.bind(null, page + 1)}
+                    >
+                        Previous
+                    </button>
+                </div>
+            </form>
+        );
+    }
+}
