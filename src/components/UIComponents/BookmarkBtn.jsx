88b661a06d6650faf95e4921eb8dc7dd49c5582e
Anton Kablukov <antoha7@gmail.com>
2020-04-06 16:19:47 +0200

    refactoring

--- src/components/UIComponents/BookmarkBtn.jsx
+++ src/components/UIComponents/BookmarkBtn.jsx
@@ -3,14 +3,16 @@ import AppContextHOC from '../HOC/AppContextHOC';
 import { Bookmark, BookmarkBorder } from '@material-ui/icons';
 
 class BookmarkBtn extends Component {
+  handleOnclick = (item) => {
+    if (this.props.session_id) {
+      this.props.addToWatchlist(item);
+    } else this.props.toggleLoginModal();
+  };
+
   render() {
     const { item, watchListMovies, addToWatchlist } = this.props;
     return (
-      <button
-        type="button"
-        className="btn"
-        onClick={() => addToWatchlist(item)}
-      >
+      <button type="button" className="btn" onClick={this.handleOnclick}>
         {watchListMovies.some((movie) => movie.id === item.id) ? (
           <Bookmark />
         ) : (
--- src/components/UIComponents/StarBtn.jsx
+++ src/components/UIComponents/StarBtn.jsx
@@ -3,15 +3,17 @@ import AppContextHOC from '../HOC/AppContextHOC';
 import { StarBorder, Star } from '@material-ui/icons';
 
 class StarBtn extends Component {
+  handleOnclick = (item) => {
+    if (this.props.session_id) {
+      this.props.addToFavorites(item);
+    } else this.props.toggleLoginModal();
+  };
+
   render() {
     const { item, favoriteMovies, addToFavorites } = this.props;
 
     return (
-      <button
-        type="button"
-        className="btn"
-        onClick={() => addToFavorites(item)}
-      >
+      <button type="button" className="btn" onClick={this.handleOnclick}>
         {favoriteMovies.some((movie) => movie.id === item.id) ? (
           <Star />
         ) : (
